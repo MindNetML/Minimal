9ec91f86d06ccee6e7626cf429890dbe0a0392f9
Jesus Alejandro Maciel <jmjamb81@users.noreply.huggingface.co>
2022-10-22 19:16:50 +0000

    Update app.py
    fixed typo on line 13

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ categories = ("Dog", "Cat")
 
 def classify_image(img):
     pred,idx,probs = learn.predict(img)
-    return dict(zip(categories, map(float,prob)))
+    return dict(zip(categories, map(float,probs)))
 
 image = gr.inputs.Image(shape=(192,192))
 label = gr.outputs.Label()
