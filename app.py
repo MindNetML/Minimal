17c78f87f75fb0c55259f4ffdc04f0f50b964cc3
Jesus Alejandro Maciel <jmjamb81@users.noreply.huggingface.co>
2022-10-22 18:51:45 +0000

    Upload 5 files
    updated app.py with code for image recognition model. Also added .pkl file with training done in kaggle.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+def is_cat(x): return x[0].isupper()
+
+learn = load_learner('model.pkl')
+
+categories = ("Dog", "Cat")
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float,prob)))
+
+image = gr.inputs.Image(shape=(192,192))
+label = gr.outputs.Label()
+
+examples = ["dog.jpg", "cat.jpg", "dunno.jpg"]
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch(inline=False)
